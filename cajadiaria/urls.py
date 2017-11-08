638143b8e597af828c50914ee2952cdfe4177eaa
mick <jmiguelcl47@gmail.com>
2017-11-08 01:18:04 -0600

    prueba5

--- cajadiaria/urls.py
+++ cajadiaria/urls.py
@@ -3,5 +3,7 @@ from . import views
 
 urlpatterns=[
     url(r'^$',views.post_list),
+        #url(r'^$', views.lista_peliculas, name ='lista_peliculas'),
     url(r'^nuevacuenta/$',views.cuenta_nueva,name='cuenta_nueva'),
+    url(r'^cuenta/(?P<pk>[0-9]+)/$',views.detallecaja,name='detallecaja')
 ]
--- cajadiaria/views.py
+++ cajadiaria/views.py
@@ -1,20 +1,29 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.contrib import messages
 from .forms import CuentaForm
 from cajadiaria.models import Cuenta,TipoCuenta,CajaDiaria
+from django.utils import timezone
+from django.shortcuts import render, get_object_or_404
+from django.contrib.auth.decorators import login_required
 
 def post_list(request):
-    return render(request,'cajadiaria/post_list.html')
+    cuenta=Cuenta.objects.all()
+    return render(request,'cajadiaria/post_list.html',{'cuenta':cuenta})
 
+def detallecaja(request,pk):
+    post=get_object_or_404(Cuenta,pk=pk)
+    return render(request,'cajadiaria/detallecaja.html',{'post':post})
+
+@login_required
 def cuenta_nueva(request):
     if request.method=="POST":
         formulario=CuentaForm(request.POST)
         if formulario.is_valid():
             cuenta=Cuenta.objects.create(nombre=formulario.cleaned_data['nombre'],fecha=formulario.cleaned_data['fecha'],descripcion=formulario.cleaned_data['descripcion'],monto=formulario.cleaned_data['monto'])
-            for cuenta_id in request.POST.getlist('cajas'):
-                cajadiaria=CajaDiaria(cuenta_id=actor_id,pelicula_id=pelicula_id)
+            for tipocuenta_id in request.POST.getlist('cajas'):
+                cajadiaria=CajaDiaria(tipocuenta_id=tipocuenta_id,cuenta_id=cuenta.id)
                 cajadiaria.save()
-            messages.add_messages(request,messages.SUCCESS,'Cuenta agregada exitosamente')
+            messages.add_message(request,messages.SUCCESS,'Cuenta agregada exitosamente')
     else:
         formulario=CuentaForm();
     return render(request,'cajadiaria/cuenta_editar.html',{'formulario':formulario})
